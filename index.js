72da3f673dd39715f9f9711ea31d1b7c1b3bef41
Rostyslav <sagotua@email.com>
2025-06-14 17:39:02 +0300

    new

---
+++ index.js
@@ -0,0 +1,27 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+// 🔐 Твій токен бота
+const token = "7928189423:AAFBsIzl18s2Niblp1BhMtptCDonMhFgAeg";
+
+// 🚀 URL на майбутній вебдодаток (тимчасово можемо поставити Google)
+const webAppUrl = "https://example.com"; // ← заміниш, коли буде готовий додаток
+
+const bot = new TelegramBot(token, { polling: true });
+
+// Відповідь на /start
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "Привіт! Готовий до тренування? 💪 Натисни кнопку нижче:", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "🚀 Старт",
+            web_app: { url: webAppUrl }
+          }
+        ]
+      ]
+    }
+  });
+});
